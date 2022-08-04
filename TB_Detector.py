a18564055c0dc0fbc498f9a22f2c6188c5e19483
Stanford Gibson <stanford.gibson@yahoo.com>
2022-08-04 12:42:01 +0200

    update

--- TB_Detector.py
+++ TB_Detector.py
@@ -102,7 +102,7 @@ def main():
         if choice == 'About':
             st.subheader("Tuberculosis (TB)")
             st.markdown("""A potentially serious infectious bacterial disease that mainly affects the lungs. \n\nThe bacteria that cause TB are spread when an infected person coughs or sneezes.            
-            Most people infected with the bacteria that cause tuberculosis don't have symptoms. \n\nWhen symptoms do occur, they usually include a cough (sometimes blood-tinged), weight loss, night sweats and fever. 
+            Most people infected with the bacteria that cause tuberculosis don't have symptoms. \n\nWhen symptoms do occur, they usually include: \n *a cough (sometimes blood-tinged), \n *weight loss, \n *night sweats and fever. 
             \n\nTreatment isn't always required for those without symptoms. Patients with active symptoms will require a long course of treatment involving multiple antibiotics.""")
             
             st.subheader("Why this app")
